decc339fe2585109af167f3eeeff69d0b7ff87ce
Gourab Bandyopadhyay <gourabbandyopadhyay.cgr@gmail.com>
2024-04-23 01:04:03 +0530

    Fixed the cloudinary issue

--- components/shared/MediaUploader.tsx
+++ components/shared/MediaUploader.tsx
@@ -53,7 +53,7 @@ const MediaUploader = ({
 
   return (
     <CldUploadWidget
-      uploadPreset="jsm_imaginify"
+      uploadPreset="gourab_sassify"
       options={{
         multiple: false,
         resourceType: "image",
